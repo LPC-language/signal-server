ac8cb7f0e9a963b5a5ac3ce6183b58edbc8dc470
Felix A. Croes <felix@dworkin.nl>
2024-06-30 20:45:42 +0200

    Preserve the credential keys generator seed.
    And export the public keys.

--- src/sys/credentials.c
+++ src/sys/credentials.c
@@ -22,8 +22,12 @@
 # include "credentials.h"
 
 private inherit "~TLS/api/lib/hkdf";
+private inherit base64 "/lib/util/base64";
 private inherit hex "/lib/util/hex";
 
+
+# define CREDENTIALS_SEED	"~/config/credentials_seed"
+# define SERVER_PUBLIC_KEYS	"~/config/ZKGROUP_SERVER_PUBLIC_PARAMS"
 
 string userDeriveKey;			/* 32 bytes */
 string key;
@@ -35,28 +39,54 @@ KeyPair profileKeyCredentialKey;
 KeyPair authCredentialWithPniKey;
 
 /*
+ * get public key from KeyPair
+ */
+private string *getPubKey(KeyPair key)
+{
+    return ({ key->C_W()->bytes(), key->I()->bytes() });
+}
+
+/*
  * credentials server
  */
 static void create()
 {
     Sho sho;
+    string generator;
+    KeyPair unused1Key, unused2Key;
+    string *keys;
 
     /* XXX */
     userDeriveKey = secure_random(32);
     key = secure_random(32);
 
     sho = PARAMS->serverSecretSho();
-    sho->absorb(secure_random(32));
+    generator = read_file(CREDENTIALS_SEED);
+    if (!generator) {
+	generator = secure_random(32);
+	write_file(CREDENTIALS_SEED, generator);
+    }
+    sho->absorb(generator);
     sho->ratchet();
 
     authCredentialKey = new KeyPair(sho, 4, 3);
-    /* profileKeyCredentialKey = */ new KeyPair(sho, 4, 4);
+    unused1Key = new KeyPair(sho, 4, 4);
     signingKey = sho->getScalar();
     publicKey = new RistrettoPoint(ristretto255_basepoint()) * signingKey;
     receiptCredentialKey = new KeyPair(sho, 4, 2);
-    /* pniCredentialKey = */ new KeyPair(sho, 6);
+    unused2Key = new KeyPair(sho, 6);
     profileKeyCredentialKey = new KeyPair(sho, 5);
     authCredentialWithPniKey = new KeyPair(sho, 5);
+
+    keys = getPubKey(authCredentialKey) +
+	   getPubKey(unused1Key) +
+	   ({ publicKey->bytes() }) +
+	   getPubKey(receiptCredentialKey) +
+	   getPubKey(unused2Key) +
+	   getPubKey(profileKeyCredentialKey) +
+	   getPubKey(authCredentialKey);
+    remove_file(SERVER_PUBLIC_KEYS);
+    write_file(SERVER_PUBLIC_KEYS, base64::encode("\0" + implode(keys, "")));
 }
 
 void initialize(string userDeriveKey, string key)
